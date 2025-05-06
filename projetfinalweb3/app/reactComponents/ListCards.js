3ee121321834fbd907c5c34043d1dababc9d3ee8
BouEmi2333222 <143424324+BouEmi2333222@users.noreply.github.com>
2025-05-06 09:49:20 -0400

    Fait une partie de accueil

---
+++ projetfinalweb3/app/reactComponents/ListCards.js
@@ -0,0 +1,18 @@
+import Card from "./Card";
+export default function ListCards()
+{
+    return(
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
+    )
+}
