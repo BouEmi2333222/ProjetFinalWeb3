b637f5b789250f7ab439409c2cdb6e3aeda82f4b
BouEmi2333222 <143424324+BouEmi2333222@users.noreply.github.com>
2025-05-15 07:59:45 -0400

    Faites le web :)

--- projetfinalweb3/app/reactComponents/ListCards.js
+++ projetfinalweb3/app/reactComponents/ListCards.js
@@ -2,17 +2,13 @@ import Card from "./Card";
 export default function ListCards()
 {
     return(
-        <div className="row row-cols-1 row-cols-md-3 g-4">
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-        </div>
+        <>
+            <h3 className="text-center">Cartes Populaires</h3>
+            <div className="row row-cols-1 row-cols-md-3 m-4 p-4 flex justify-content-center border border-color-primary">
+                {Array.from({ length: 9 }).map((_, index) => (
+                    <Card key={index} />
+                ))}
+            </div>
+        </>
     )
 }
