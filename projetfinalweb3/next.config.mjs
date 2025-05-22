5a1af6c60e9e3595edda92232d32c80771a37933
BouEmi2333222 <143424324+BouEmi2333222@users.noreply.github.com>
2025-05-22 19:31:11 -0400

    added api connection

--- projetfinalweb3/next.config.mjs
+++ projetfinalweb3/next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://projet-prog4e04.cegepjonquiere.ca:443/:path*',
+          },
+        ]
+    },
+}
+
+export default nextConfig
