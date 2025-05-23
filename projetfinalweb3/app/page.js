5a83fbfef07aec87a82862bf92c92024d8cad0db
BouEmi2333222 <143424324+BouEmi2333222@users.noreply.github.com>
2025-05-22 20:29:42 -0400

    Rajouter nos dossiers components

--- projetfinalweb3/app/page.js
+++ projetfinalweb3/app/page.js
@@ -1,17 +1,28 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./reactComponents/Header.js"
 import 'bootstrap/dist/css/bootstrap.css';
 import Footer from "./reactComponents/Footer";
-import Card from "./reactComponents/Card";
-import Carousel from "./reactComponents/Carousel";
-import ListCards from "./reactComponents/ListCards";
+import Card from "./reactComponents/CarteProduit";
+import Carousel from "./reactComponents/Emile/Carousel";
+import ListCards from "./reactComponents/ListesProduits";
+import React from "react"
 
 export default function Home() {
+  const [cartes, setCartes] = React.useState([])
+    React.useEffect(() => {
+        async function fetchPosts() { 
+            const response = await fetch(`https://projet-prog4e04.cegepjonquiere.ca:443/api/Booster`)
+            var json = await response.json()
+            setCartes(json)
+        }
+        fetchPosts()
+    }, [])
   return (<>
     <Header/>
     <Carousel/>
-    <ListCards/>
+    <ListCards produits={cartes}/>
     <Footer/>
     </>
   )
